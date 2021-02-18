8412a4c8c0afdd8d240198bf52673cd4c62546f1
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-02-18 15:26:21 +0100

    Install & setup Material UI & Remark transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-theme-material-ui',
+      options: {
+        stylesConfig: {
+          // disableAutoprefixing: true,
+          // disableMinification: true,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
+  ],
 }
---
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {},
+});
+
+export default theme;
+
