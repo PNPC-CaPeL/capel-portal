cbecd59db6241e2fdeae0f4d38b1eaba1963bf44
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-02-19 16:12:53 +0100

    Define default theme colors

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,7 +1,14 @@
 import { createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme({
-  palette: {},
+  palette: {
+    primary: {
+      main: '#3991aa',
+    },
+    secondary: {
+      main: '#f0373d',
+    },
+  },
 });
 
 export default theme;
