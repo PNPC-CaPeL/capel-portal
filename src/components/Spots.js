17a843c0f827cb71aad8d9ec808bdae3f3b990a3
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-03-23 12:00:03 +0100

    Manage ability to store favorite spots in browser localStorage

--- src/components/Spots.js
+++ src/components/Spots.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { GeoJSON, Popup } from 'react-leaflet';
 import { marker, icon } from 'leaflet';
 import { Button } from 'gatsby-theme-material-ui';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+import createPersistedState from 'use-persisted-state';
 
 import MarkdownText from './MarkdownText';
 import useSpots from '../hooks/useSpots';
@@ -22,8 +26,11 @@ const pointToLayer = (geoJsonPoint, latlng) =>
     }),
   });
 
+const useFavs = createPersistedState('capel-favs');
+
 const Spots = ({ popupComponent: CustomPopup, ...props }) => {
   const spots = useSpots();
+  const [favs, setFavs] = useFavs({});
 
   if (!spots || !spots.length) {
     return null;
@@ -43,6 +50,9 @@ const Spots = ({ popupComponent: CustomPopup, ...props }) => {
           properties: { name, ...frontmatter },
         };
 
+        const isFav = favs[spot.name];
+        const StarComponent = isFav ? StarIcon : StarBorderIcon;
+
         return (
           <GeoJSON
             key={name}
@@ -53,6 +63,11 @@ const Spots = ({ popupComponent: CustomPopup, ...props }) => {
           >
             {(typeof CustomPopup === 'undefined') && (
               <Popup>
+                <StarComponent
+                  onClick={() => setFavs(({ [spot.name]: isPrevFav, ...prevFavs }) =>
+                    (isPrevFav ? prevFavs : { ...prevFavs, [spot.name]: true }))}
+                  color={isFav && 'secondary'}
+                />
                 <MarkdownText hast={htmlAst} />
                 <Button onClick={() => console.log(spot)} variant="outlined" color="secondary">
                   Déclarer une plongée ici.
