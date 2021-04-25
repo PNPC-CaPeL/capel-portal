500ff108522eead841c6aa9f04d0a78454fd6409
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-04-26 00:21:06 +0200

    Setup social network icons

--- src/components/SNButton.js
+++ src/components/SNButton.js
@@ -1,42 +1,50 @@
 import React from 'react';
-import clsx from 'clsx';
 
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import facebook from '../assets/facebook.svg';
+import twitter from '../assets/twitter.svg';
+import instagram from '../assets/instagram.svg';
+import youtube from '../assets/youtube.svg';
+
+const picto = {
+  facebook,
+  twitter,
+  instagram,
+  youtube,
+};
+
 const links = {
   facebook: { href: 'https://perdu.com' },
   twitter: { href: 'https://perdu.com' },
+  instagram: { href: 'https://perdu.com' },
+  youtube: { href: 'https://perdu.com' },
   default: { href: 'https://perdu.com' },
 };
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   link: {
     display: 'inline-block',
     textDecoration: 'none',
   },
 
-  circle: {
-    background: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-
-    borderRadius: '100%',
+  picto: {
+    display: 'block',
     width: 70,
     height: 70,
-
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-}));
+});
 
-const SNButton = ({ type = 'default', className, ...props }) => {
+const SNButton = ({ type = 'default', ...props }) => {
   const classes = useStyles();
 
   if (!Object.keys(links).includes(type)) {
     // eslint-disable-next-line no-param-reassign
     type = 'default';
   }
+
+  const Picto = picto[type];
 
   return (
     <a
@@ -45,8 +53,8 @@ const SNButton = ({ type = 'default', className, ...props }) => {
       className={classes.link}
       {...links[type]}
     >
-      <Box className={clsx(classes.circle, className)} {...props}>
-        sn
+      <Box {...props}>
+        <Picto className={classes.picto} />
       </Box>
     </a>
   );
