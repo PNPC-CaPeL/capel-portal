5c22fbb86927d68edca037b2f3746ecc160806cb
Mathieu DARTIGUES <mathieu@dartic.fr>
2021-05-27 11:47:20 +0200

    Update src/pages/inscription.js
    Co-authored-by: mab <benjamin.marguin@makina-corpus.com>

--- src/pages/inscription.js
+++ src/pages/inscription.js
@@ -32,7 +32,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function validateEmail (email) {
+const validateEmail = email => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
