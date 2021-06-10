271dfa146c6d0017d6e00bb74db29be23dac1b2c
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-06-10 20:17:41 +0200

    Cleanup plugin gatsby-plugin-ghost-pages

--- plugins/gatsby-plugin-ghost-pages/gatsby-node.js
+++ plugins/gatsby-plugin-ghost-pages/gatsby-node.js
@@ -18,46 +18,19 @@ exports.onCreateNode = ({ node, actions: { createNodeField } }) => {
 
 /**
  * Here is the place where Gatsby creates the URLs for all the
- * posts, tags, pages and authors that we fetched from the Ghost site.
+ * posts and pages that we fetched from the Ghost site.
  */
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions;
-
+exports.createPages = async ({
+  graphql,
+  actions: { createPage, createRedirect },
+}) => {
   const result = await graphql(`
     {
       allGhostPost(sort: { order: ASC, fields: published_at }) {
-        edges {
-          node {
-            slug
-            url
-          }
-        }
-      }
-      allGhostTag(sort: { order: ASC, fields: name }) {
-        edges {
-          node {
-            slug
-            url
-            postCount
-          }
-        }
-      }
-      allGhostAuthor(sort: { order: ASC, fields: name }) {
-        edges {
-          node {
-            slug
-            url
-            postCount
-          }
-        }
+        nodes { slug url }
       }
       allGhostPage(sort: { order: ASC, fields: published_at }) {
-        edges {
-          node {
-            slug
-            url
-          }
-        }
+        nodes { slug url }
       }
     }
   `);
@@ -68,82 +41,19 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   // Extract query results
-  // const tags = result.data.allGhostTag.edges;
-  // const authors = result.data.allGhostAuthor.edges;
-  const pages = result.data.allGhostPage.edges;
-  const posts = result.data.allGhostPost.edges;
+  const pages = result.data.allGhostPage.nodes;
+  const posts = result.data.allGhostPost.nodes;
 
   // Load templates
-  // const indexTemplate = path.resolve('./src/templates/index.js');
-  // const tagsTemplate = path.resolve('./src/templates/tag.js');
-  // const authorTemplate = path.resolve('./src/templates/author.js');
   const pageTemplate = path.resolve('./src/components/templates/GhostPage.js');
   const postTemplate = path.resolve('./src/components/templates/GhostPost.js');
 
-  /*
-  // Create tag pages
-  tags.forEach(({ node }) => {
-    const totalPosts = node.postCount !== null ? node.postCount : 0;
-
-    // This part here defines, that our tag pages will use
-    // a `/tag/:slug/` permalink.
-    const url = `/tag/${node.slug}`;
-
-    const items = Array.from({ length: totalPosts });
-
-    // Create pagination
-    paginate({
-      createPage,
-      items,
-      itemsPerPage: postsPerPage,
-      component: debugTemplate,
-      pathPrefix: ({ pageNumber }) => ((pageNumber === 0) ? url : `${url}/page`),
-      context: {
-        slug: node.slug,
-      },
-    });
-  });
-  */
-
-  /*
-  // Create author pages
-  authors.forEach(({ node }) => {
-    const totalPosts = node.postCount !== null ? node.postCount : 0;
-
-    // This part here defines, that our author pages will use
-    // a `/author/:slug/` permalink.
-    const url = `/author/${node.slug}`;
-
-    const items = Array.from({ length: totalPosts });
-
-    // Create pagination
-    paginate({
-      createPage,
-      items,
-      itemsPerPage: postsPerPage,
-      component: debugTemplate,
-      pathPrefix: ({ pageNumber }) => ((pageNumber === 0) ? url : `${url}/page`),
-      context: {
-        slug: node.slug,
-      },
-    });
-  });
-  */
-
   // Create pages
-  pages.forEach(({ node }) => {
-    // This part here defines, that our pages will use
-    // a `/:slug/` permalink.
-    // node.url = `/${node.slug}/`;
-
+  pages.forEach(node => {
     createPage({
       path: `/${node.slug}/`,
       component: pageTemplate,
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.slug,
-      },
+      context: { slug: node.slug },
     });
 
     // Redirection from `/edit` to Ghost edit page
@@ -155,19 +65,11 @@ exports.createPages = async ({ graphql, actions }) => {
   });
 
   // Create post pages
-  posts.forEach(({ node }) => {
-    // This part here defines, that our posts will use
-    // a `/:slug/` permalink.
-    // node.url = `/${node.slug}/`;
-
+  posts.forEach(node => {
     createPage({
       path: `/${node.slug}/`,
       component: postTemplate,
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.slug,
-      },
+      context: { slug: node.slug },
     });
 
     // Redirection from `/edit` to Ghost edit page
@@ -177,20 +79,4 @@ exports.createPages = async ({ graphql, actions }) => {
       toPath: `${node.url}edit`,
     });
   });
-
-  /*
-  // Create pagination
-  paginate({
-    createPage,
-    items: posts,
-    itemsPerPage: postsPerPage,
-    component: debugTemplate,
-    pathPrefix: ({ pageNumber }) => {
-      if (pageNumber === 0) {
-        return '/';
-      }
-      return '/page';
-    },
-  });
-  */
 };
