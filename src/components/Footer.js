a7167c7009c2a10fe9084398c1a9880f716b04eb
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-06-10 15:08:02 +0200

    Fix footer items alignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,26 +8,32 @@ import HtmlAstRender from './HtmlAstRender';
 import SNButton from './SNButton';
 import useFooterBlocks from '../hooks/useFooterBlocks';
 
-const useStyles = makeStyles(theme => ({
-  social: {
-    background: `linear-gradient(0deg, ${theme.palette.secondary.main} 50%, transparent 50%);`,
-  },
+const useStyles = makeStyles(theme => {
+  const minWidth = mq => `@media (min-width: ${theme.breakpoints.values[mq]}px)`;
 
-  footer: {
-    background: theme.palette.secondary.main,
-    color: theme.palette.secondary.contrastText,
-    paddingBottom: theme.spacing(4),
-    paddingTop: theme.spacing(4),
-
-    '& a': {
-      color: 'inherit',
+  return {
+    social: {
+      background: `linear-gradient(0deg, ${theme.palette.secondary.main} 50%, transparent 50%);`,
     },
-  },
-  block: {
-    '&:nth-child(2)': { textAlign: 'center' },
-    '&:nth-child(3)': { textAlign: 'right' },
-  },
-}));
+
+    footer: {
+      background: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText,
+      paddingBottom: theme.spacing(4),
+      paddingTop: theme.spacing(4),
+
+      '& a': {
+        color: 'inherit',
+      },
+    },
+    block: {
+      [minWidth('md')]: {
+        '&:nth-child(2)': { textAlign: 'center' },
+        '&:nth-child(3)': { textAlign: 'right' },
+      },
+    },
+  };
+});
 
 const Footer = ({ className, ...props }) => {
   const classes = useStyles();
