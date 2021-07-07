bd01dedcf1a19edc482a5cab5beaf186372700d1
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-07-07 18:52:47 +0200

    Create legend for spots

--- src/components/MapLegend.jsx
+++ src/components/MapLegend.jsx
@@ -22,24 +22,92 @@ const useStyles = makeStyles(theme => ({
   label: {
     fontSize: '0.9rem',
   },
+
+  icon: {
+    maxWidth: 24,
+    maxHeight: 24,
+    verticalAlign: 'middle',
+  },
+
+  iconLabel: {
+    whiteSpace: 'nowrap',
+    fontSize: '0.9rem',
+    '& span': {
+      display: 'inline-block',
+      width: 28,
+    },
+  },
 }));
+
+const icons = [
+  {
+    file: '/diving-mask.svg',
+    label: 'Spot (naturel)',
+  },
+  {
+    file: '/shipwreck.svg',
+    label: 'Spot (épave)',
+  },
+  {
+    file: '/structure2.svg',
+    label: 'Partenaire',
+  },
+];
 
 const MapLegend = ({ className, ...props }) => {
   const classes = useStyles();
 
   return (
-    <Grid container spacing={1} className={clsx(classes.root, className)} {...props}>
-      {styles.map(([properties, style]) => (
-        <Grid item className={classes.item} key={JSON.stringify(properties)}>
-          <Typography variant="body2" className={classes.label}>
-            <svg width="20" height="20">
-              <path d="M2.5 2.5h15v15H2.5z" {...style2svg(style)} />
-            </svg>
+    <Grid
+      container
+      className={clsx(classes.root, className)}
+      {...props}
+    >
+      <Grid
+        item
+        xs={12}
+        md={9}
+        container
+        spacing={1}
+      >
+        {styles.map(([properties, style]) => (
+          <Grid item className={classes.item} key={JSON.stringify(properties)}>
+            <Typography variant="body2" className={classes.label}>
+              <svg width="20" height="20">
+                <path d="M2.5 2.5h15v15H2.5z" {...style2svg(style)} />
+              </svg>
 
-            {Object.values(properties).join(', ').replace('ammarage', 'amarrage')}
-          </Typography>
-        </Grid>
-      ))}
+              {Object.values(properties).join(', ').replace('ammarage', 'amarrage')}
+            </Typography>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        md={3}
+        container
+        spacing={1}
+        alignItems="start"
+      >
+        {icons.map(({ file, label }) => (
+          <Grid item key={label} className={classes.item} xs={12}>
+            <Typography
+              variant="body2"
+              className={classes.iconLabel}
+            >
+              <span>
+                <img
+                  src={file}
+                  className={classes.icon}
+                  alt={label}
+                />
+              </span> {label}
+            </Typography>
+          </Grid>
+        ))}
+      </Grid>
     </Grid>
   );
 };
