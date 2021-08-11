3be6caa2a3fcd18d60a889f249caab585a0c3347
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-08-11 18:07:39 +0200

    Add parsed value to lckSettings when text_value is JSON

--- src/hooks/useLckSettings.js
+++ src/hooks/useLckSettings.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 export const useLckSettings = () => {
@@ -12,9 +13,19 @@ export const useLckSettings = () => {
     }
   `);
 
+  const parsedNodes = React.useMemo(() => nodes.map(node => {
+    try {
+      return { ...node, value: JSON.parse(node.text_value) };
+    } catch (err) {
+      // not a JSON value
+    }
+
+    return node;
+  }), [nodes]);
+
   return [
-    nodes,
-    nodes.reduce((acc, node) => ({ ...acc, [node.key]: node }), {}),
+    parsedNodes,
+    parsedNodes.reduce((acc, node) => ({ ...acc, [node.key]: node }), {}),
   ];
 };
 
